51385a55d787de92b4e8ab8dc2f108a6213f808e
yoogukhyeon <rnrgus5897@gmail.com>
2022-12-12 20:29:00 +0900

    김다슬 숙제

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Heeader = styled.div`
+  width: 100%;
+  height: 200px;
+  background-color: #222;
+`;
 
 export default function Header() {
   return (
-    <div>
+    <Heeader>
       <h2>This is Header</h2>
-    </div>
+
+      {/* 
+        1. navi 
+        2. react-router-dom V6 Router 정리해서 설명 
+        3. styled-components UI를 만든 파일에서 css 사용 
+        4. home, community /community, login /login
+      */}
+    </Heeader>
   );
 }
