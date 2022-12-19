8a3493c263ae2541c79ca11dc03689c1ac748806
yoogukhyeon <rnrgus5897@gmail.com>
2022-12-19 14:02:24 +0900

    json-server test & board meta tags

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import MetaTag from '../constants/SEOMetaTag';
 
 function Board() {
-    return <div>Board</div>;
+    return (
+        <>
+            <MetaTag title="게시판" description="게시판 목록" />
+            <div>Board</div>
+        </>
+    );
 }
 
 export default Board;
